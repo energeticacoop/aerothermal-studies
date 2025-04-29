c0ee48fa2b2e09c1ab7a1dc1387cbbed07582817
juaniten <juanguijarroferreiro@gmail.com>
2025-04-29 22:07:58 +0200

    restore code from master GAS

--- src/OnOpen.js
+++ src/OnOpen.js
@@ -1,15 +1,28 @@
-// V_1.0.0 18/04/25
 function onOpen() {
   const ui = SpreadsheetApp.getUi()
-  ui
-    .createMenu("🔥 Estudios calefacción")
+  ui.createMenu("🔥 Estudios calefacción")
     .addItem("🖇️ Importar base de datos de materiales", "importDb")
-    .addItem("📂 Generar enlaces a directorios de documentación", "printAllFoldersLinks")
+    .addItem(
+      "📂 Generar enlaces a directorios de documentación",
+      "printAllFoldersLinks"
+    )
     .addSeparator()
-    .addItem("📘 Generar estudio de aerotermia con radiadores", "generateRadiatorAE")
-    .addItem("📘 Generar estudio de aerotermia con suelo radiante", "generateUnderfloorAE")
-    .addItem("📘 Generar estudio de aerotermia nueva o sin consumos con radiadores", "generateNewRadiatorAE")
-    .addItem("📘 Generar estudio de aerotermia nueva o sin consumos con suelo radiante", "generateNewUnderfloorAE")
+    .addItem(
+      "📘 Generar estudio de aerotermia con radiadores",
+      "generateRadiatorAE"
+    )
+    .addItem(
+      "📘 Generar estudio de aerotermia con suelo radiante",
+      "generateUnderfloorAE"
+    )
+    .addItem(
+      "📘 Generar estudio de aerotermia nueva o sin consumos con radiadores",
+      "generateNewRadiatorAE"
+    )
+    .addItem(
+      "📘 Generar estudio de aerotermia nueva o sin consumos con suelo radiante",
+      "generateNewUnderfloorAE"
+    )
     .addSeparator()
     .addItem("📕 Generar estudio de biomasa con caldera", "generateBoiler")
     .addItem("📕 Generar estudio de biomasa con hidroestufa", "generateWater")
@@ -24,7 +37,6 @@ function onOpen() {
     .addToUi()
 
   ui.createMenu("🤖 Utilidades")
-    //.addItem("💣 Borrar todos los campos de la pestaña actual", "eraseNamedFields")
     .addItem("💣 Borrar campos de salida de documentos", "forgetFolders")
     .addToUi()
 }
@@ -34,35 +46,83 @@ function checkDatabase() {
 }
 
 function generateRadiatorAE() {
-  createDocuments(getStudyTemplates("1p1RCsYWr8oNgr0-DnO_ky3kBp2gx30n2XLW1SzKCUeY", "Estudio aerotermia con radiadores"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1p1RCsYWr8oNgr0-DnO_ky3kBp2gx30n2XLW1SzKCUeY",
+      "Estudio aerotermia con radiadores"
+    ),
+    "outputStudy"
+  )
 }
 
 function generateUnderfloorAE() {
-  createDocuments(getStudyTemplates("1-w_4EUponNr7bUgH1Uu_vb7yZrzr4eqDiFfHFgewggU", "Estudio aerotermia con suelo radiante"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1-w_4EUponNr7bUgH1Uu_vb7yZrzr4eqDiFfHFgewggU",
+      "Estudio aerotermia con suelo radiante"
+    ),
+    "outputStudy"
+  )
 }
 
 function generateNewRadiatorAE() {
-  createDocuments(getStudyTemplates("1lpB_g7nBTmFW3AdXYW1tqfR1JnBzt7wJskrK50RXLM0", "Estudio aerotermia con radiadores"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1lpB_g7nBTmFW3AdXYW1tqfR1JnBzt7wJskrK50RXLM0",
+      "Estudio aerotermia con radiadores"
+    ),
+    "outputStudy"
+  )
 }
 
 function generateNewUnderfloorAE() {
-  createDocuments(getStudyTemplates("1Ll7ZkD-G-OMMB4xScdg2iL3Hknq9vChNZijglCGDMWc", "Estudio aerotermia con suelo radiante"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1Ll7ZkD-G-OMMB4xScdg2iL3Hknq9vChNZijglCGDMWc",
+      "Estudio aerotermia con suelo radiante"
+    ),
+    "outputStudy"
+  )
 }
 
 function generateBoiler() {
-  createDocuments(getStudyTemplates("1EGQtUqyQOeT_bw1r89AYe2C3SyEis0mLyPPKiQZiKso", "Estudio biomasa con caldera"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1EGQtUqyQOeT_bw1r89AYe2C3SyEis0mLyPPKiQZiKso",
+      "Estudio biomasa con caldera"
+    ),
+    "outputStudy"
+  )
 }
 
 function generateWater() {
-  createDocuments(getStudyTemplates("1cvyP1Esg56g_UUQK10RGilOjGYVNO8wGRWKRhWxj1r4", "Estudio biomasa con hidroestufa"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1cvyP1Esg56g_UUQK10RGilOjGYVNO8wGRWKRhWxj1r4",
+      "Estudio biomasa con hidroestufa"
+    ),
+    "outputStudy"
+  )
 }
 
 function generateHeater() {
-  createDocuments(getStudyTemplates("1gHNhuA3e2Te1_IKMwyo0LwAqoiz4cDfokKshoyTPOSI", "Estudio biomasa con estufa"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1gHNhuA3e2Te1_IKMwyo0LwAqoiz4cDfokKshoyTPOSI",
+      "Estudio biomasa con estufa"
+    ),
+    "outputStudy"
+  )
 }
 
 function generateFire() {
-  createDocuments(getStudyTemplates("1XYiZCwf2By3XAQSQvdHH7LsK_6DTAXDvFDljurjGJxU", "Estudio biomasa con leña"), "outputStudy")
+  createDocuments(
+    getStudyTemplates(
+      "1XYiZCwf2By3XAQSQvdHH7LsK_6DTAXDvFDljurjGJxU",
+      "Estudio biomasa con leña"
+    ),
+    "outputStudy"
+  )
 }
 
 function createDocumentation() {
@@ -72,6 +132,3 @@ function createDocumentation() {
 function createContract() {
   createDocuments(getContractTemplate(), "outputContract")
 }
-
-
-
--- src/Rite.js
+++ src/Rite.js
@@ -1,15 +1,14 @@
-/**
- * Creates an MGE certificate using Energética's API fill-mge-form endpoint.
- */
-function createMGEcertificate() {
+function createRITEmemory() {
   try {
     // Define the FastAPI endpoint URL
-    const fastApiUrl = 'https://api.energetica.coop/fill-mge-form/'
+    const fastApiUrl = "https://api.energetica.coop/fill-rite-form/"
 
     // Define the payload data
-    const namedRanges = SpreadsheetApp.getActiveSpreadsheet().getNamedRanges()
+    const namedRanges = SpreadsheetApp.getActiveSpreadsheet()
+      .getSheetByName("Un equipo")
+      .getNamedRanges()
     const replacementValues = namedRanges.reduce((acc, namedRange) => {
-      const keyWithBraces = `<${namedRange.getName()}>`
+      const keyWithBraces = `{${namedRange.getName()}}`
       acc[keyWithBraces] = namedRange.getRange().getDisplayValue()
       return acc
     }, {})
@@ -17,9 +16,9 @@ function createMGEcertificate() {
 
     // Make a POST request to the FastAPI endpoint
     const response = UrlFetchApp.fetch(fastApiUrl, {
-      method: 'POST',
+      method: "POST",
       payload: JSON.stringify(payload),
-      contentType: 'application/json',
+      contentType: "application/json",
     })
 
     // Check if the response status is OK
@@ -28,15 +27,18 @@ function createMGEcertificate() {
       const blob = response.getBlob()
 
       // Get the destination folder
-      const destinationFolder = getDestinationFolder("folder0205")
+      const destinationFolder = getDestinationFolder("folder02")
 
       // Create a file in the same folder with a specific name
-      const fileName = `${get("nombreCompletoPromotor")} - Certificado MGE.pdf`
+      const fileName = `${getValue("nombre")} ${getValue(
+        "apellidos"
+      )} - Memoria RITE.pdf`
       Tools.deleteFile(fileName, destinationFolder)
       const file = destinationFolder.createFile(blob.setName(fileName))
 
       // Output link to document
-      setURL("outputMGE", file.getUrl(), "Certificado MGE")
+      setURL(getRangeByName("outputRITE"), file.getUrl(), "Memoria RITE")
+      SpreadsheetApp.flush()
 
       // Log the file URL
       Logger.log(`File URL: ${file.getUrl()}`)
