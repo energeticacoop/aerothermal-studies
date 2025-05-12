ea0d9140298bdd2783397b35ae2cc11ac5c02fa6
carlos <carlos@energetica.coop>
2025-05-12 18:23:17 +0200

    update (lun 12 may 2025 18:23:17 CEST)

---
+++ src/version.js
@@ -0,0 +1,8 @@
+function version() {
+    // v1.0.1 30/04/25 Estudio calefacción (aerotermia y biomasa) automatizado
+    // DEV doc: 1_t6gjMlkGe3f27JxvWu3rG0CzvHh08-jdQLWyX1rupo
+    // DEV scr: 1cQLl6UFKILPxqT0HJJIpJFhcXs0Yc3ynCAAVG8CAlMBQgQwaxNrI8nK4
+    // PROD doc: 1-fIJzDFh6x2furcWNv8ANu3B6suPbeupphZa_2q38zw
+    // PROD scr: 1pIUv4S-WHFTUSgxqKyGQ8uxwQJ5fFVFXL1w0Y4xRXS2OPns0_fMris6H
+  }
+  
