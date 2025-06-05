31cf12b2c1509ea71d4aa999f59c0a99a814c837
carlos <carlos@energetica.coop>
2025-06-05 14:19:08 +0200

    update (jue 05 jun 2025 14:19:08 CEST)

--- src/version.js
+++ src/version.js
@@ -1,9 +1,23 @@
 function version() {
     // v1.0.1 30/04/25 Estudio calefacción (aerotermia y biomasa) automatizado
-    // fld: 1bH_RmIkldxa5IJztaf1kaiwi561MT9w6
+    // v1.0.2 01/06/25 Add en Master {direccionymas} to the template [Modelo_Memoria_RITE.pdf]
+    // v1.0.3 05/06/25 Add en Master "Reforma", {ref_catastral}, {eq_xx}, {Refrigerante, datos}, {Rendimiento} to the template [Modelo_Memoria_RITE.pdf]
+
+
+
+    // fld: 1bH_RmIkldxa5IJztaf1kaiwi561MT9w6  //  80 - Servicios Energéticos  -  zz. Estudios calefacción automatizados (máster)
     // DEV doc: 1_t6gjMlkGe3f27JxvWu3rG0CzvHh08-jdQLWyX1rupo
     // DEV scr: 1cQLl6UFKILPxqT0HJJIpJFhcXs0Yc3ynCAAVG8CAlMBQgQwaxNrI8nK4
-    // PROD doc: 1-fIJzDFh6x2furcWNv8ANu3B6suPbeupphZa_2q38zw
-    // PROD scr: 1pIUv4S-WHFTUSgxqKyGQ8uxwQJ5fFVFXL1w0Y4xRXS2OPns0_fMris6H
+    // PROD doc: 1-fIJzDFh6x2furcWNv8ANu3B6suPbeupphZa_2q38zw   // Máster] Estudio calefacción (aerotermia y biomasa) automatizado
+    // PROD scr: 1pIUv4S-WHFTUSgxqKyGQ8uxwQJ5fFVFXL1w0Y4xRXS2OPns0_fMris6H    //   Estudios Aerotermia
+    
+    // known isuues
+    // 05/06/25
+    // Memoria RITE en pdf: al sustituir los campos clave por sus valores de la hoja, problemas con algunas poblaciones en los desplegables de población (Renedo, Laguna...)
+
+
   }
   
+
+    // pdte update in Github
+    // pdte update DEV
